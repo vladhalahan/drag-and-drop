572a0b2462541184cfccab4d5f6d6e6d1c3dd14b
Vlad <vhalahan@simulmedia.com>
2025-01-22 19:51:59 +0200

    Splitted to modules

---
+++ src/components/project-item.ts
@@ -0,0 +1,40 @@
+import Component from "./base-component.js";
+import { Project } from "../models/project.js";
+import { Draggable } from "../models/drag-drop.js";
+import { autobind } from "../decorators/autobind.js";
+
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+  constructor(hostId: string, private project: Project) {
+    super('single-project', hostId, false, project.id)
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler)
+    this.element.addEventListener('dragend', this.dragEndHandler)
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id)
+    event.dataTransfer!.effectAllowed = 'move';
+  };
+
+  @autobind
+  dragEndHandler(_: DragEvent) {};
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person'
+    } else {
+      return `${this.project.people} persons`
+    }
+  }
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
