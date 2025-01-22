0330f004288e9151dc6d257e3ab23b4910cf94e6
Vlad <vhalahan@simulmedia.com>
2025-01-22 21:01:46 +0200

    Added webpack

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import Component from "./base-component.js";
-import { Project } from "../models/project.js";
-import { Draggable } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind.js";
+import Component from "./base-component";
+import { Project } from "../models/project";
+import { Draggable } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind";
 
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
   constructor(hostId: string, private project: Project) {
